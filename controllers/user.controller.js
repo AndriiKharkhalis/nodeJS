0d5e5ae6f5aa33fd17e878e5873e2cbbb2693814
AndriiKharkhalis <akharkhalis@gmail.com>
2023-04-28 00:24:14 +0200

    ESLint

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,3 @@
-const { userInfo } = require('os');
 const { findById } = require('../dataBase/User.model');
 const User = require('../dataBase/User.model');
 const ApiError = require('../error/ApiError');
@@ -6,102 +5,101 @@ const ApiError = require('../error/ApiError');
 
 module.exports = {
 
-   createUser: async (req, res, next) => {
-      try {
-         const createUser = await User.create(req.body);
+  createUser: async (req, res, next) => {
+    try {
+      const createUser = await User.create(req.body);
 
-         res.status(201).json(createUser);
-      } catch (e) {
-         next(e);
+      res.status(201).json(createUser);
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  getAllUsers: async (req, res, next) => {
+    try {
+      const { limit = 20, page = 1 } = req.query;
+      const skip = (page - 1) * limit;
+
+      const users = await User.find().limit(limit).skip(skip);
+      const count = await User.count();
+
+      res.render('users',
+        {
+          page,
+          perPage: limit,
+          data: users,
+          count
+        }
+      );
+
+      // res.json({
+      //    page,
+      //    perPage: limit,
+      //    data: users,
+      //    count
+      // });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  getUserById: async (req, res, next) => {
+    try {
+      const { userIndex } = req.params;
+      const user = await User.findById(userIndex);
+
+      // const { userIndex } = req.params;
+      // const user = DBusers[userIndex];
+      // Without async/await - ERR_HTTP_HEADERS_SENT
+      // Without try/catch - when processing !user, the answer will be null
+
+      if (!user) {
+        next(new ApiError(`User with ID ${userIndex} not found`, 400));
+        return;
       }
-   },
+      res.json(user);
 
-   getAllUsers: async (req, res, next) => {
-      try {
-         const { limit = 20, page = 1 } = req.query;
-         const skip = (page - 1) * limit;
+    } catch (e) {
+      next(e);
+    }
+  },
 
-         const users = await User.find().limit(limit).skip(skip);
-         const count = await User.count();
+  deleteUser: async (req, res, next) => {
+    try {
+      const { userIndex } = req.params;
+      const deletedUser = await User.findOneAndDelete({ _id: userIndex }); // якщо deleteOne - то тре вказати, що буде тим ід
 
-         res.render('users',
-            {
-               page,
-               perPage: limit,
-               data: users,
-               count
-            }
-         );
-
-         // res.json({
-         //    page,
-         //    perPage: limit,
-         //    data: users,
-         //    count
-         // });
-      } catch (e) {
-         next(e);
+      if (!deletedUser) {
+        next(new ApiError(`User ${userIndex} not found`, 404));
+        return;
       }
-   },
 
-   getUserById: async (req, res, next) => {
-      try {
-         const { userIndex } = req.params;
-         const user = await User.findById(userIndex);
+      res.json({ 
+        message: `User ${userIndex} deleted`, 
+        deletedUser: deletedUser 
+      });
 
-         // const { userIndex } = req.params;
-         // const user = DBusers[userIndex];
-         // Without async/await - ERR_HTTP_HEADERS_SENT
-         // Without try/catch - when processing !user, the answer will be null
+    } catch (e) {
+      next(e);
+    }
+  },
 
-         if (!user) {
-            next(new ApiError(`User with ID ${userIndex} not found`, 400));
-            return;
-         }
-         res.json(user);
+  updateUser: async (req, res, next) => {
+    try {
+      const { userIndex } = req.params;
+      const updateUser = await User.findOneAndUpdate({ _id: userIndex }, { $set: req.body });
+      // const user = DBusers[userIndex];
+      // Object.assign(DBusers[userIndex], req.body);
 
-      } catch (e) {
-         next(e);
+      if (!updateUser) {
+        next(new ApiError(`User with ID ${ userIndex } not found`, 404));
+        return;
       }
-   },
 
-   deleteUser: async (req, res, next) => {
-      try {
-         const { userIndex } = req.params;
-         const deletedUser = await User.findOneAndDelete({ _id: userIndex }); // якщо deleteOne - то тре вказати, що буде тим ід
+      res.json(updateUser);
 
-         if (!deletedUser) {
-            next(new ApiError(`User ${userIndex} not found`, 404));
-            return;
-         }
-
-         res.json({ 
-            message: `User ${userIndex} deleted`, 
-            deletedUser: deletedUser 
-        });
-
-      } catch (e) {
-         next(e);
-      }
-   },
-
-
-   updateUser: async (req, res, next) => {
-      try {
-         const { userIndex } = req.params;
-         const updateUser = await User.findOneAndUpdate({ _id: userIndex }, { $set: req.body });
-         // const user = DBusers[userIndex];
-         // Object.assign(DBusers[userIndex], req.body);
-
-         if (!updateUser) {
-            next(new ApiError(`User with ID ${ userIndex } not found`, 404));
-            return;
-         }
-
-         res.json(updateUser);
-
-      } catch (e) {
-         next(e);
-      }
-   }
+    } catch (e) {
+      next(e);
+    }
+  }
 }
--- dataBase/User.model.js
+++ dataBase/User.model.js
@@ -1,17 +1,16 @@
-const { timeStamp } = require('console'); // appeared by itself
-
 const { Schema, model } = require('mongoose');
 
 const userRolesEnum = require('../constants/user-roles.enum');
 
 const User = new Schema(
-    {
-        name: { type: String, trim: true, required: true },
-        mail: { type: String, trim: true, lowercase: true, unique: true, required: true }, // required - means must be
-        age: { type: Number, default: 18 },
-        role: { type: String, enum: Object.values(userRolesEnum), default: userRolesEnum.USER }
-    },
-    { timestamps: true } // created at & update at in DB
+  {
+    name: { type: String, trim: true, required: true },
+    mail: { type: String, trim: true, lowercase: true, unique: true, required: true }, // required - means must be
+    age: { type: Number, default: 18 },
+    role: { type: String, enum: Object.values(userRolesEnum), default: userRolesEnum.USER }
+  },
+  { timestamps: true } // created at & update at in DB
 );
 
-module.exports = model('User', User); // 'User' - the name of the model, User - the scheme on the basis of which we build the model
+module.exports = model('User', User); 
+// 'User' - the name of the model, User - the scheme on the basis of which we build the model
